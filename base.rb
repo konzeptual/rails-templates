3c26b8fbdda6dc054c4f68a022813107eee2dbf4
Caleb Land <caleb.land@gmail.com>
2009-06-11 15:46:08 -0400

    use gems of rspec and rspec-rails, not plugins

--- base.rb
+++ base.rb
@@ -1,6 +1,6 @@
 if yes?("Do you want to use RSpec for testing?")
-  plugin "rspec", :git => "git://github.com/dchelimsky/rspec.git"
-  plugin "rspec-rails", :git => "git://github.com/dchelimsky/rspec-rails.git"
+  gem "rspec"
+  gem "rspec-rails"
   generate :rspec
 end
 
