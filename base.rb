ceaf1c50db7093cef9343278824e2e2a1d7d3769
krick <antipin.konstantin@googlemail.com>
2009-11-20 06:58:52 +0800

    add factory girl gem.

--- base.rb
+++ base.rb
@@ -3,6 +3,7 @@ gem "rspec",       :lib => false, :version => ">= 1.2.9",  :environment => :test
 gem "rspec-rails", :lib => false, :version => ">= 1.2.9",  :environment => :test
 gem "cucumber",    :lib => false, :version => ">=0.3.11",  :environment => :test
 gem "webrat",      :lib => false, :version => ">=0.4.4",   :environment => :test
+gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com", :environment => :test
 
 generate :rspec
 generate :cucumber
