3e1849a19417d4527a6bb1e110dee5a580f2cafb
krick <antipin.konstantin@googlemail.com>
2009-11-20 08:44:50 +0800

    add route, needed for launching_soon

--- launching-soon.rb
+++ launching-soon.rb
@@ -4,6 +4,7 @@ plugin "launching-soon", :git => "git://github.com/vinsol/Launching-Soon.git"
 # UNIX only
 run "sed -i '/^class/a \\\ \\\ include LaunchingSoon if Rails.env.production?' app/controllers/application_controller.rb"
 
+route "map.resources :news_letter_subscribers, :only => [:create]"  
 route "map.root :controller => :application"  
 
 rake "db:migrate:launching_soon",  :env => 'production'
