363a852a821c609181ad2b41e121ba240cc7857f
krick <antipin.konstantin@googlemail.com>
2009-11-20 08:13:12 +0800

    add launching-soon template.

---
+++ launching-soon.rb
@@ -0,0 +1,7 @@
+plugin "launching-soon", :git => "git://github.com/vinsol/Launching-Soon.git"
+
+# add 'include LaunchingSoon' to the application_controller
+# UNIX only
+run "sed -i '/^class/a \\\ \\\ include LaunchingSoon if Rails.env.production?' app/controllers/application_controller.rb"
+
+route "map.root :controller => :application"  
