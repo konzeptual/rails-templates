dd3e9b838b8c1df44ab77399da151966a299a69f
krick <antipin.konstantin@googlemail.com>
2009-11-20 08:54:34 +0800

    rename custom_templates to my_new_shiny_application.

--- my_new_shiny_application.rb
+++ my_new_shiny_application.rb
@@ -1,7 +1,7 @@
 def path_to_template(template_name)
   "http://github.com/krick/rails-templates/raw/master/#{template_name}.rb"
-
 end
 
 load_template path_to_template('base')
-load_template path_to_template('authentication')
+load_template path_to_template('launching-soon')
+# load_template path_to_template('authentication')
