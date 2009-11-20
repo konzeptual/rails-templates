cdfa733226448622c78803a4be05be67bb8e1e7a
krick <antipin.konstantin@googlemail.com>
2009-11-20 08:26:59 +0800

    add migrations needed for launching-soon

--- launching-soon.rb
+++ launching-soon.rb
@@ -5,3 +5,5 @@ plugin "launching-soon", :git => "git://github.com/vinsol/Launching-Soon.git"
 run "sed -i '/^class/a \\\ \\\ include LaunchingSoon if Rails.env.production?' app/controllers/application_controller.rb"
 
 route "map.root :controller => :application"  
+
+rake "db:migrate:launching_soon"
